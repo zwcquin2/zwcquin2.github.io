652ff267da71a51d2a78a5201292f52a9005539d
zwcquin <skullscraper889@gmail.com>
2024-04-27 18:42:46 +0200

    projects sectie toegevoegd

--- js/main.js
+++ js/main.js
@@ -5,11 +5,11 @@ const navbar = document.querySelector(".navbar");
 
 //nu zorg ik ervoor dat de navbar een bg kleur krijgt wanneer je scrollt
 window.addEventListener('scroll', () => {
-    //wanneer de gebruiker 150px naar beneden scrollt, wordt de bg toegevoegd
-    if (window.scrollY >= 150) {
+    //wanneer de gebruiker 120px naar beneden scrollt, wordt de bg toegevoegd
+    if (window.scrollY >= 120) {
         navbar.classList.add('navbar-scrolled');
         //wanneer de gebruiker op height: 0 zit gaat de bg weer weg :)
-    } else if (window.scrollY < 150) {
+    } else if (window.scrollY < 120) {
         navbar.classList.remove('navbar-scrolled')
     }
 });
