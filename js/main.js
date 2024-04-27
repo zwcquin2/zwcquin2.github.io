6828fe44c00e958120fcb1028204e1901328be03
zwcquin <skullscraper889@gmail.com>
2024-04-27 14:28:16 +0200

    bg color on scroll gegeven aan de navbar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 console.log("File Loaded");
+
+const nav = document.querySelector(".navbar");
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= 150) {
+        nav.classList.add('navbar-scrolled');
+    } else if (window.scrollY < 150) {
+        nav.classList.remove('navbar-scrolled')
+    }
+});
